a5ee2d3cfb175183e72513350b9f767c22f67d16
Aachukihipl <aachukiagarwal.hipl@gmail.com>
2024-11-05 11:59:54 +0530

    importing leads

--- app/Imports/LeadsImport.php
+++ app/Imports/LeadsImport.php
@@ -16,7 +16,7 @@ class LeadsImport implements ToCollection, WithHeadingRow
         // return $rows;
 
         foreach ($rows as $row) {
-            $registrationDate = \Carbon\Carbon::createFromFormat('Y-m-d', '1900-01-01')->addDays($row['registration_date'] - 2)->format('Y-m-d H:i:s');
+            $registrationDate = !empty($row['registration_date']) ? \Carbon\Carbon::createFromFormat('Y-m-d', '1900-01-01')->addDays($row['registration_date'] - 2)->format('Y-m-d H:i:s') : null;
 
             $birthDate = !empty($row['birth_date']) ? \Carbon\Carbon::createFromFormat('Y-m-d', '1900-01-01')->addDays($row['birth_date'] - 2)->format('Y-m-d') : null;
 
@@ -31,43 +31,43 @@ class LeadsImport implements ToCollection, WithHeadingRow
             $identificationTypeArrEs = __('cruds.identification_type');
 
 
-            $civilStatusValue =  $row['civil_status'] ? ucfirst(strtolower(trim($row['civil_status']))) : '';
+            $civilStatusValue =  isset($row['civil_status']) ? ucfirst(strtolower(trim($row['civil_status']))) : '';
             $civil_status = $civilStatusValue ? getKeyByValue('civil_status', $civilStatusArr, $civilStatusArrEs, $civilStatusValue) : null;
 
 
-            $employmentStatusValue =  $row['employment_status'] ? ucfirst(strtolower(trim($row['employment_status']))) : '';
+            $employmentStatusValue =  isset($row['employment_status']) ? ucfirst(strtolower(trim($row['employment_status']))) : '';
             $employment_status = $employmentStatusValue ? getKeyByValue('employment_status', $employmentStatusArr, $employmentStatusArrEs, $employmentStatusValue) : null;
 
 
-            $identificationTypeValue =  $row['identification'] ? ucfirst(strtolower(trim($row['identification']))) : '';
+            $identificationTypeValue =  isset($row['identification']) ? ucfirst(strtolower(trim($row['identification']))) : '';
             $identification_type = $identificationTypeValue ? getKeyByValue('identification_type', $identificationTypeArr, $identificationTypeArrEs, $identificationTypeValue) : null;
 
             $gender = isset($row['gender']) ? ($row['gender'] === 'F' ? 1 : ($row['gender'] === 'M' ? 2 : null)) : null;
 
 
             $createData = [
-                'name'                  => $row['first_name'] ? trim($row['first_name']) : null,
-                'last_name'             => $row['last_name']  ? trim($row['last_name']) : null,
-                'email'                 => $row['email']      ? trim($row['email']) : null,
-                'phone'                 => $row['phone']      ? trim($row['phone']) : null,
-                'cellphone'             => $row['cellphone']  ? trim($row['cellphone']) : null,
-                'identification'        => $row['identification_number']  ? trim($row['identification_number']) : null,
-                'identification_type'   => $identification_type,   //getIdentificationType($row['identification']),
+                'name'                  => isset($row['first_name']) ? trim($row['first_name']) : null,
+                'last_name'             => isset($row['last_name'])  ? trim($row['last_name']) : null,
+                'email'                 => isset($row['email'])      ? trim($row['email']) : null,
+                'phone'                 => isset($row['phone'])      ? trim($row['phone']) : null,
+                'cellphone'             => isset($row['cellphone'])  ? trim($row['cellphone']) : null,
+                'identification'        => isset($row['identification_number'])  ? trim($row['identification_number']) : null,
+                'identification_type'   => $identification_type,
                 'birthdate'             => $birthDate,
                 'gender'                => $gender,
                 'civil_status'          => $civil_status,
-                'province'              => $row['province'] ? $row['province'] : null,
-                'city'                  => $row['city'] ? $row['city'] :  null,
-                'address'               => $row['street_address_intersection'] ?  $row['street_address_intersection'] : null,
-                'sector'                => $row['sector'] ? $row['sector'] : null,
-                'reference'             => $row['reference'] ?  $row['reference'] :  null,
+                'province'              => isset($row['province']) ? $row['province'] : null,
+                'city'                  => isset($row['city']) ? $row['city'] :  null,
+                'address'               => isset($row['street_address_intersection']) ?  $row['street_address_intersection'] : null,
+                'sector'                => isset($row['sector']) ? $row['sector'] : null,
+                'reference'             => isset($row['reference']) ?  $row['reference'] :  null,
                 'employment_status'     => $employment_status,
                 'social_security'       => null,
-                'company_name'          => $row['company_name'] ?  $row['company_name'] : null,
-                'occupation'            => $row['occupation'] ?  $row['occupation'] : null,
-                'area_id'               => Null,
-                'campaign_id'           => Null,
-                'created_at'            => $registrationDate,
+                'company_name'          => isset($row['company_name']) ?  $row['company_name'] : null,
+                'occupation'            => isset($row['occupation']) ?  $row['occupation'] : null,
+                'area_id'               => null,
+                'campaign_id'           => null,
+                'created_at'            => $registrationDate ?? null,
             ];
 
             Lead::create($createData);
